1827efa257d7885e7e03103b11042b1497275e35
Jon Ramm <jonwramm@gmail.com>
2023-01-20 11:08:50 -0500

    add client folder structure, abstract Home page

--- client/App.js
+++ client/App.js
@@ -2,36 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import Home from './pages/Home';
 
 export default function App() {
 
-  const [greeting, setGreeting] = useState("Hello World!");
-
-  useEffect(() => {
-    const getHello = async () => {
-      try{
-        const res = await axios.get('http://192.168.1.37:3000/')
-        setGreeting(res.data)
-      } catch(error) {
-        console.log(error)
-      }
-    }
-    getHello()
-  })
-
   return (
-    <View style={styles.container}>
-      <Text>{greeting}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Home/>
+  )
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
