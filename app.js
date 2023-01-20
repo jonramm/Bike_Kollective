6c5c5695e3525457cf6a5b7b9a37b1224bb6929c
jonargee19 <jrgbone@gmail.com>
2023-01-20 13:20:14 -0500

    firestore connection test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const db = require('./src/config/db')
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -10,6 +11,14 @@ app.use(
     extended: true,
   })
 );
+
+async function getTest () {
+  const snapshot = await db.collection('test').get();
+  snapshot.forEach((doc) => {
+    console.log(doc.id, '=>', doc.data());
+});
+}
+getTest()
 
 const testRouter = require('./src/routes/test.route');
 
