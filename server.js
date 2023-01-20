827f4e33b634ce373b1c7bbe7c5fbde6f525fe2a
jonargee19 <jrgbone@gmail.com>
2023-01-20 14:55:47 -0500

    add ip to server logs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 const port = process.env.PORT || 3000
+const ip = require('ip');
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+  console.log(`Listening at ${ip.address()}, port ${port}`)
 })
