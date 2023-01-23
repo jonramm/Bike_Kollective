4d34ee01a58c80e32493237a6c2224c78e6aed65
Jon Ramm <jonwramm@gmail.com>
2023-01-23 17:56:34 -0500

    removed express server and related tests in root directory

--- src/routes/test.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const testController = require('../controllers/test.controller');
-
-/* GET test. */
-router.get('/', testController.get);
-
-module.exports = router;
--- tests/hello.test.js
+++ tests/hello.test.js
@@ -1,24 +1,4 @@
-const request = require("supertest");
-const app = require("../app");
-
-describe("Test the root path status", () => {
-  test("Status code 200", done => {
-    request(app)
-      .get("/")
-      .then(response => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-  });
-});
-
-describe("Test the root path response", () => {
-  test("Response is 'It Worked!", done => {
-    request(app)
-      .get("/")
-      .then(response => {
-        expect(response.text).toBe("It Worked!");
-        done();
-      });
-  });
+// Generic test
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4);
 });
