2d372d70b4397c3cd701f4dad1772f6f7fc1c12e
Jon Ramm <89990113+jonramm@users.noreply.github.com>
2023-01-24 11:18:24 -0500

    map displays general location

--- client/App.tsx
+++ client/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Home from './pages/Home';
+import Map from "./pages/Map";
 
 export default function App() {
 
   return (
-    <Home/>
+    <Map/>
   )
 
 }
---
+++ client/pages/Map.tsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import { StatusBar } from 'expo-status-bar';
+import { 
+    StyleSheet, 
+    Text, 
+    View, 
+    ScrollView, 
+    Image, 
+    TextInput,
+    FlatList 
+} from 'react-native';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    map: {
+      width: '100%',
+      height: '100%',
+    },
+  });
+
+const Map = () => {
+
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        (async () => {   
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            } 
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+      }, []);
+
+    if (location) {
+        return (
+            <View style={styles.container}>
+                <MapView 
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: location.coords.latitude,
+                        longitude: location.coords.longitude,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                      }} />
+            </View>  
+        )
+    } else {
+        return (
+            <View>
+                <Text>Loading...</Text>
+            </View>
+        )
+    }
+    
+}
+
+export default Map;
