0f3a2758dfe57db12664fa708b8b148c4cbc37b0
lyanita <anitaly.ubc@gmail.com>
2023-01-25 17:21:42 -0800

    Config files to import .env values to react app

---
+++ client/types/env.d.tsx
@@ -0,0 +1,9 @@
+declare module '@env' {
+    export const REACT_APP_FIREBASE_KEY: string;
+    export const REACT_APP_FIREBASE_DOMAIN: string;
+    export const REACT_APP_FIREBASE_DATABASE: string;
+    export const REACT_APP_FIREBASE_PROJECT_ID: string;
+    export const REACT_APP_FIREBASE_STORAGE_BUCKET: string;
+    export const REACT_APP_FIREBASE_SENDER_ID: string;
+    export const REACT_APP_FIREBASE_APP_ID: string;
+}
