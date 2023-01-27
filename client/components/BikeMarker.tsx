6b6f6ac58f383ed0cda14636cdf764fd667716a4
jonargee19 <jrgbone@gmail.com>
2023-01-27 11:52:27 -0500

    bikes display custom text box with check out button

--- client/components/BikeMarker.tsx
+++ client/components/BikeMarker.tsx
@@ -1,7 +1,7 @@
-import { Marker } from 'react-native-maps';
+import { Marker, Callout, CalloutSubview } from 'react-native-maps';
 import {  BikeProp } from '../types';
 import MarkerImage from './MarkerImage';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Alert, View, Text, Pressable, Button } from 'react-native';
 
 const bikeImage = require('../assets/bike.png')
 
@@ -14,12 +14,26 @@ const BikeMarker = (props: BikeProp) => {
                 'longitude': bike.location.longitude
             }}
             title={bike.name}
-            description={bike.status}
         >
             <MarkerImage
                 img={bikeImage}
                 styles={styles}
             />
+            <Callout
+                alphaHitTest
+                style={styles.customView}
+            >
+                <View>
+                    <Text>{bike.name}</Text>
+                    <Text>{bike.status}</Text>
+                    <Button 
+                        title='Check Out Bike!'
+                        onPress={_ => {
+                            Alert.alert('button pressed');
+                            }}
+                    />
+                </View>
+            </Callout>
         </Marker>
     )
 }
@@ -29,6 +43,12 @@ const styles = StyleSheet.create({
       width: 40,
       height: 40,
     },
+    customView: {
+        width: 140,
+        height: 140,
+        textAlign: 'center',
+        justifyContent: 'center'
+      },
   });
 
 export default BikeMarker;
