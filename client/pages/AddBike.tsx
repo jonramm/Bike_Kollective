8885bd14e18cc1deda52aa23ce4cf7ff4e28753f
jonargee19 <jrgbone@gmail.com>
2023-01-31 13:47:40 -0500

    app fetches user info from db and displays greeting in Home screen

--- client/pages/AddBike.tsx
+++ client/pages/AddBike.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet } from 'react-native';
+import { firebase, auth } from '../configs/firebase';
+import axios from "axios";
 
 const AddBike = () => {
+
+    console.log(auth.currentUser.uid);
+
     return (
         <View style={styles.container}>
             <Text>Add Bike</Text>
--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { View, Text, Button, StyleSheet, Pressable, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/core'
+import { getUser } from "../services/users";
 
 import { auth } from "../configs/firebase";
 
 const Home = ({navigation}) => {
-    // const navigation = useNavigation();
+    
+    const [user, setUser] = useState({
+        first_name: '',
+        last_name: '',
+        email: ''
+    });
+
     const handleSignOut = () => {
         auth
           .signOut()
@@ -14,39 +22,52 @@ const Home = ({navigation}) => {
           })
           .catch(error => alert(error.message))
     }
-    return (
-        <View 
-            style={[
-                styles.container
-            ]}>
-            <Text style={styles.text}>Welcome to Bike Kollective!</Text>
-            <Pressable
-                style={styles.button}
-                onPress={() =>
-                    navigation.navigate('Map')
-                }
-            >
-                <Text style={styles.buttonText}>Map</Text>
-            </Pressable>
 
-            <Pressable
-                style={styles.button}
-                onPress={() =>
-                    navigation.navigate('AddBike')
-                }
-            >
-                <Text style={styles.buttonText}>Add Bike</Text>
-            </Pressable>
+    useEffect(() => {
+        getUser(auth.currentUser.uid)
+            .then(data => setUser(data))
+            .catch(err => console.log(err));
+    }, []);
 
-            <Text>Email: {auth.currentUser?.email}</Text>
-            <TouchableOpacity
-                onPress={handleSignOut}
-                style={styles.button}
-            >
-                <Text style={styles.buttonText}>Logout</Text>
-            </TouchableOpacity>
-        </View>
-    )
+    if (user) {
+        return (
+            <View 
+                style={[
+                    styles.container
+                ]}>
+                <Text style={styles.text}>Welcome to Bike Kollective, {user.first_name}!</Text>
+                <Pressable
+                    style={styles.button}
+                    onPress={() =>
+                        navigation.navigate('Map')
+                    }
+                >
+                    <Text style={styles.buttonText}>Map</Text>
+                </Pressable>
+    
+                <Pressable
+                    style={styles.button}
+                    onPress={() =>
+                        navigation.navigate('AddBike')
+                    }
+                >
+                    <Text style={styles.buttonText}>Add Bike</Text>
+                </Pressable>
+    
+                <Text>Email: {auth.currentUser?.email}</Text>
+                <TouchableOpacity
+                    onPress={handleSignOut}
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonText}>Logout</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    } else {
+        navigation.replace("Login");
+    }
+
+    
 }
 
 const styles = StyleSheet.create({
@@ -63,7 +84,7 @@ const styles = StyleSheet.create({
     button: {
         alignItems: 'center',
         justifyContent: 'center',
-        marginTop: 12,
+        marginVertical: 10,
         paddingVertical: 12,
         paddingHorizontal: 32,
         borderRadius: 4,
