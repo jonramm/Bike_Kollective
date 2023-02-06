1fe08fdfd7037d0218f099a8dcc85a3989ce410b
lyanita <anitaly.ubc@gmail.com>
2023-02-05 21:00:54 -0800

    Add lock combo, time remaining and end trip views

--- client/pages/ReturnBike.tsx
+++ client/pages/ReturnBike.tsx
@@ -1,32 +1,30 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, FlatList} from 'react-native';
+import { Text, View, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import BikeItem from '../components/BikeItem';
-import { rides } from '../data/testRides'; // switch to db/axios, use parameters
-import { bikes } from '../data/testBikes'; 
+import { getBikes } from "../services/bikes";
+import { getRides } from "../services/rides";
 
 const ReturnBike = ({navigation}) => {
     const [uid, setUid] = useState('');
     const [bike, setBike] = useState([]);
     const [ride, setRide] = useState([]);
+    const [lockCombo, setlockCombo] = useState('');
 
     // get uid from local storage
     const getUid = async () => {
         const uid = await AsyncStorage.getItem('uid');
-        console.log(uid);
         setUid(uid);
         return uid;
     };
 
     // grab current trip for the user
     const getRide = async (uid: string) => {
-        console.log(rides);
-        console.log(uid);
-        var result = rides.filter(function(val) {
+        const rides = await getRides();
+        var result = await rides.filter(function(val) {
             return (val["rider"] == uid && val.end_time == null);
         });
-        console.log(result);
         setRide(result);
         return result;
     };
@@ -34,18 +32,29 @@ const ReturnBike = ({navigation}) => {
     // grab bike associated with current trip
     const getBike = async (ride: string | any[]) => {
         if (ride.length > 0) {
+            const bikes = await getBikes();
             var result = bikes.filter(function(val) {
                 return (val["bike_id"] == ride[0].bike);
             });
-            console.log(result);
             setBike(result);
+            return result;
         }
     }
+
+    const getLockCombo = async (bike: string | any[]) => {
+        if (bike.length > 0) {
+            const lockCombo = await bike[0].lock_combo;
+            setlockCombo(lockCombo);
+            return lockCombo;
+        }
+    };
 
     useEffect(() => {
         getUid().then(uid => {
             getRide(uid).then(ride => {
-                getBike(ride);
+                getBike(ride).then(bike => {
+                    getLockCombo(bike);
+                });
             });
         });
     }, []);
@@ -53,14 +62,38 @@ const ReturnBike = ({navigation}) => {
     // if a ride is checked out by the user
     if (ride.length > 0) {
         return (
-            <View style={styles.bikesContainer}>
-                <Text>Trip in Progress</Text>
-                <FlatList style={styles.bikesWrapper}
-                    keyExtractor={item => item.bike_id}
-                    data={bike}
-                    renderItem={({item}) => (<BikeItem name={item.name} photo={item.photo}></BikeItem>)}
-                />
-            </View>
+                <View style={styles.container}>
+                    <View style={styles.componentContainer}>
+                        <Text style={styles.header}>Trip in Progress</Text>
+                    </View>
+                    <FlatList style={styles.bikesWrapper}
+                        keyExtractor={item => item.bike_id}
+                        data={bike}
+                        renderItem={({item}) => (<BikeItem name={item.name} photo={item.photo}></BikeItem>)}
+                    />
+
+                    <View style={styles.componentContainer}>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.subHeader}>Lock Combination</Text>
+                        </View>
+                        <View style={styles.comboContainer}>
+                            <Text style={styles.comboText}>{lockCombo}</Text>
+                        </View>
+                    </View>
+
+                    <View style={styles.componentContainer}>
+                        <Text style={styles.subHeader}>Time Remaining</Text>
+                    </View>
+
+                    <View style={styles.componentContainer}>
+                        <View style={styles.buttonContainer}>
+                            <TouchableOpacity onPress={() => alert('Todo: add end trip function')} style={styles.button}>
+                                <Text style={styles.buttonText}>End Trip</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                    
+                </View>
         )
     // if no ride is checked out by the user
     } else {
@@ -74,14 +107,82 @@ const ReturnBike = ({navigation}) => {
 }
 
 const styles = StyleSheet.create({
-    bikesContainer: {
+    container: {
         flex: 1,
         backgroundColor: '#FFF',
-      },
-      bikesWrapper: {
+    },
+    textContainer: {
+        flex: 1,
+        backgroundColor: '#FFF',
+        flexDirection: 'column',
+    },
+    header: {
+        position: 'absolute',
+        width: 310,
+        height: 86,
+        left: 33,
+        top: 56,
+        fontStyle: 'normal',
+        fontWeight: '700',
+        fontSize: 30,
+        lineHeight: 41,
+        textAlign: 'justify',
+        color: '#3F3D56',
+    },
+    subHeader: {
+        width: 296,
+        height: 30,
+        fontStyle: 'normal',
+        fontWeight: '700',
+        fontSize: 18,
+        lineHeight: 25,
+        textAlign: 'justify',
+        color: '#3F3D56',
+    },
+    componentContainer: {
+        flex: 4,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        textAlign: 'justify',
+    },
+    bikesWrapper: {
         paddingTop: 20,
         paddingHorizontal: 20,
+    },
+    buttonContainer: {
+        width: '60%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40,
       },
+    button: {
+        backgroundColor: '#00BFA6',
+        width: '100%',
+        padding: 15,
+        borderRadius: 20,
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 16,
+    },
+    comboContainer: {
+        flex: 0.5,
+        backgroundColor: '#F2F2F2',
+        paddingVertical: 5,
+        paddingHorizontal: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    comboText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
 })
 
 export default ReturnBike;
