aa7fcb0fdde44b8c2a155ba39099d873676f69c1
jonargee19 <jrgbone@gmail.com>
2023-02-09 10:42:25 -0500

    add bike list overlay button to map

--- client/components/Map.tsx
+++ client/components/Map.tsx
@@ -1,4 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { 
+    Pressable, 
+    Text,
+    TouchableOpacity, 
+    View } from "react-native";
 import MapView from 'react-native-maps';
 import { 
     StyleSheet, 
@@ -26,30 +31,51 @@ const Map = (props: LocationProps) => {
     }, []);
 
     return (
-        <MapView 
-            style={styles.map}
-            region={{
-                latitude: props.latitude,
-                longitude: props.longitude,
-                latitudeDelta: 0.0222,
-                longitudeDelta: 0.0221,
-                }} 
-            showsUserLocation={true}
-        >
-            {bikeArray.map((bike) => {
-                return <BikeMarker 
-                            bike={bike} 
-                            key={bike.bike_id}
-                            userLocation={userLocation} />
-            })}
-        </MapView>
+        <View style={styles.mapContainer}>
+            <MapView 
+                style={styles.map}
+                region={{
+                    latitude: props.latitude,
+                    longitude: props.longitude,
+                    latitudeDelta: 0.0222,
+                    longitudeDelta: 0.0221,
+                    }} 
+                showsUserLocation={true}
+            >
+                {bikeArray.map((bike) => {
+                    return <BikeMarker 
+                                bike={bike} 
+                                key={bike.bike_id}
+                                userLocation={userLocation} />
+                })}
+            </MapView>
+            <TouchableOpacity 
+                style={styles.listButton}
+                >
+                <Text>List View</Text>
+            </TouchableOpacity>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
+    mapContainer: {
+        ...StyleSheet.absoluteFillObject,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
     map: {
       width: '100%',
       height: '100%',
+    },
+    listButton: {
+        position: "absolute", 
+        top: 20,
+        backgroundColor: 'white',
+        width: '40%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
     }
   });
 
