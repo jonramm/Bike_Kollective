1402077d849da4bc8571e25ff5a469724467013e
jonargee19 <jrgbone@gmail.com>
2023-02-13 14:24:56 -0500

    add map toggle button to list page

--- client/pages/ListBikes.tsx
+++ client/pages/ListBikes.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { 
-    View, 
-    StyleSheet, 
-    FlatList, 
+import {
+    View,
+    StyleSheet,
+    FlatList,
     Text,
     SafeAreaView,
-    StatusBar 
+    StatusBar,
+    TouchableOpacity
 } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import BikeItem from '../components/BikeItem';
 import { getBikes } from "../services/bikes";
 import DropDownPicker from 'react-native-dropdown-picker';
@@ -20,12 +22,12 @@ const ListBikes = () => {
 
     const [bikeArray, setBikeArray] = useState([]);
     const [selectedBikes, setSelectedBikes] = useState([]);
-    
+
     // for dropdown picker
     const [open, setOpen] = useState(false);
     const [items, setItems] = useState(tagData);
     const [tags, setTags] = useState([]);
-    
+
     useEffect(() => {
         getBikes()
             .then(data => {
@@ -45,45 +47,67 @@ const ListBikes = () => {
                 return parentArray.includes(x)
             })
         };
-        if (tags){
+        if (tags) {
             setSelectedBikes([...bikeArray].filter((x) => checkSubset(x.tags, tags) === true));
         }
-        else{
+        else {
             setSelectedBikes(bikeArray);        // Reset the bike list because no tags are selected
         }
     };
 
     return (
         <SafeAreaView style={styles.bikesContainer}>
-            <StatusBar 
+            <StatusBar
                 backgroundColor='white'
                 barStyle='dark-content'
             />
-            <View style={styles.dropdownWrapper}>
-                <Text style={styles.dropdownLabel}>Filter Search by Tags</Text>
-                <DropDownPicker
-                    maxHeight={300}
-                    style={styles.dropdownInput}
-                    multiple={true}
-                    min={0}
-                    max={3}
-                    open={open}
-                    value={tags}
-                    items={items}
-                    setOpen={setOpen}
-                    setValue={setTags}
-                    setItems={setItems}
-                    mode='BADGE'
-                    onChangeValue={filterBikes}
-                    showBadgeDot={false}
+            <View style={styles.searchRow}>
+                <View style={styles.dropdownWrapper}>
+                    <Text style={styles.dropdownLabel}>Filter Search by Tags</Text>
+                    <DropDownPicker
+                        maxHeight={300}
+                        style={styles.dropdownInput}
+                        multiple={true}
+                        min={0}
+                        max={3}
+                        open={open}
+                        value={tags}
+                        items={items}
+                        setOpen={setOpen}
+                        setValue={setTags}
+                        setItems={setItems}
+                        mode='BADGE'
+                        onChangeValue={filterBikes}
+                        showBadgeDot={false}
                     />
+                </View>
+                <TouchableOpacity
+                    // Had to apply type 'never' to string param for navigate.
+                    // Seems like a weird React/TypeScript issue and this is
+                    // a quick workaround as found here:
+                    // https://stackoverflow.com/questions/68667766/react-native-typescript-string-is-not-assignable-to-parameter-of-type-never
+                    onPress={() => {
+                        navigation.navigate(
+                            'Search' as never,
+                            { screen: 'Map' } as never
+                        )
+                    }
+                    }
+                >
+                    <Ionicons
+                        name='map'
+                        size={40}
+                        color='black'
+                    />
+                </TouchableOpacity>
             </View>
+
             <FlatList style={styles.bikesWrapper}
                 ListEmptyComponent={handleEmpty}
                 keyExtractor={item => item.bike_id}
                 data={selectedBikes}            // TODO: filter by distance
                 extraData={selectedBikes}
-                renderItem={({item}) => (<BikeItem bike={item} hasLink={true}></BikeItem>)}
+                renderItem={({ item }) => (<BikeItem bike={item} hasLink={true}></BikeItem>)}
             />
         </SafeAreaView>
     )
@@ -93,7 +117,7 @@ const styles = StyleSheet.create({
     bikesContainer: {
         flex: 1,
         backgroundColor: '#FFF',
-      },
+    },
     dropdownWrapper: {
         paddingTop: 30,
         paddingHorizontal: 20,
@@ -116,6 +140,13 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginBottom: 5,
     },
+    searchRow: {
+        flexDirection: 'row',
+        width: '65%',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        zIndex: 100
+    }
 })
 
 export default ListBikes;
