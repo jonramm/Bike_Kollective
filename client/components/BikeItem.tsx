5643202a2f892c1ce18cbc16c7b0a5644bd9e2ac
schaefkr <schaefkr@oregonstate.edu>
2023-02-16 16:46:06 -0800

    added base.tsx and styles.tsx and updated BikeInfo.tsx, BikeItem.tsx and ListBikes.tsx

--- client/components/BikeItem.tsx
+++ client/components/BikeItem.tsx
@@ -8,6 +8,9 @@ import { useNavigation } from "@react-navigation/native";
 import {AuthContext} from '../navigation/AuthProvider';
 import { distToBike } from '../services/distanceCalc';
 import { FirebaseImgProps } from '../types/types';
+import {styles} from '../styles/styles';
+import {colors, iconSizes} from '../styles/base';
+
 
 const BikeItem = (props) => {
 
@@ -31,18 +34,18 @@ const BikeItem = (props) => {
             bike: bike,
             userLocation: userLocation
           } as never)}} >
-      <Icon name='chevron-right' size={20} style={styles.bikeItemIcon} />
+      <Icon name='chevron-right' size={iconSizes.md} style={[styles.iconGrayDark, styles.itemRowSpaceRight]} />
     </TouchableOpacity>
   ) : null;
 
   return (
-    <View style={styles.bikeItem}>
-      <View style={styles.bikeItemLeft}>
+    <View style={styles.cardContainer}>
+      <View style={styles.containerRowsFlexWrap}>
         <FirebaseImg photo={bike.photo} imgProps={imgProps}></FirebaseImg>
-        <View style={styles.bikeItemCenter}>
-          <Text style={styles.bikeItemText}>{bike.name}</Text>
-          <Rating stars={bike.agg_rating} maxStars={5} size={20} color={'#00BFA6'}/>
-          <Text>{distToBike(userLocation, bike.location)} meters away</Text>
+        <View style={styles.containerColsXSmall}>
+          <Text style={styles.headerMedium}>{bike.name}</Text>
+          <Rating stars={bike.agg_rating} maxStars={5} size={20} color={colors.green}/>
+          <Text style={styles.textSmall}>{distToBike(userLocation, bike.location)} meters away</Text>
         </View>
       </View>
         {hasBikeInfoLink}
@@ -50,33 +53,33 @@ const BikeItem = (props) => {
   )
 }
 
-const styles = StyleSheet.create({
-    bikeItem: {
-    backgroundColor: '#F2F2F2',
-    padding: 15,
-    borderRadius: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginBottom: 20,
-  },
-  bikeItemLeft: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    flexWrap: 'wrap'
-  },
-  bikeItemCenter: {
-    flexDirection: 'column',
-  },
-  bikeItemText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 5,
-  },
-  bikeItemIcon: {
-    color: '#d6d7da',
-    marginRight: 10,
-  },
-});
+// const styles = StyleSheet.create({
+//   bikeItem: {
+//     backgroundColor: '#F2F2F2',
+//     padding: 15,
+//     borderRadius: 10,
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     justifyContent: 'space-between',
+//     marginBottom: 20,
+//   },
+//   bikeItemLeft: {
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     flexWrap: 'wrap'
+//   },
+//   bikeItemCenter: {
+//     flexDirection: 'column',
+//   },
+//   bikeItemText: {
+//     fontSize: 18,
+//     fontWeight: 'bold',
+//     marginBottom: 5,
+//   },
+//   bikeItemIcon: {
+//     color: '#d6d7da',
+//     marginRight: 10,
+//   },
+// });
 
 export default BikeItem;
