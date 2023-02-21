3388edb598ffd43428c34fd5dd12d9c5916d9ead
jonargee19 <jrgbone@gmail.com>
2023-02-21 15:21:07 -0500

    add title splash screen

--- client/navigation/AuthStack.tsx
+++ client/navigation/AuthStack.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
+import TitleScreen from '../pages/TitleScreen';
 
 const Stack = createStackNavigator();
 
 const AuthStack = () => {
+
+  const [showTitle, setShowTitle] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowTitle(false);
+    }, 3 * 1000)
+  }, []);
+
+  if (showTitle) {
+    return <TitleScreen />
+  }
+
   return (
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Login" component={Login} />
